eedb5a718e7004fc019a17f59b952adfbac02d70
master <master@clevertech.ru>
2012-09-11 19:11:37 +0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oteststore::Application.config.secret_token = 'f2cfea924e306b1f91b82987dbe6ef3788a1b8709cfc02c7646f621c1b969836466d31e70c1425bbb17aec152bb278edbe7370e6ca1191a5903b2d9bd136bb4e'
